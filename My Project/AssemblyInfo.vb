3cd59b640a24ba8658b0c27f05d72c342d57eb0a
Madc30 <62174789+Madc30@users.noreply.github.com>
2020-04-30 15:06:39 +0200

    Add files via upload

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+' zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+' powiązane z zestawem.
+
+' Sprawdź wartości atrybutów zestawu
+
+<Assembly: AssemblyTitle("PC UltraClean Pro Antivirus Plus Two Optimize")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("PC UltraClean Pro Antivirus Plus Two Optimize")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Następujący identyfikator GUID jest identyfikatorem elementu typelib w przypadku udostępnienia tego projektu w modelu COM
+<Assembly: Guid("d59a9b67-efee-482c-b129-03a2b86e022a")>
+
+' Informacje o wersji zestawu zawierają następujące cztery wartości:
+'
+'      Wersja główna
+'      Wersja pomocnicza
+'      Numer kompilacji
+'      Poprawka
+'
+' Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+' przy użyciu symbolu „*”, tak jak pokazano poniżej:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
